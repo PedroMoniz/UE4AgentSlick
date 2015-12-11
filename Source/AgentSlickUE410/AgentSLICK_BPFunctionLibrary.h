3b1942236764a3fd7c05dd0e8ac277b2ba1bd41d
PedroMoniz <pedromoniz22@gmail.com>
2015-12-11 17:46:48 +0000

    Added CSV for the guard talking with you in tuxedo.
    Answears now come from the csv but it is not being processed the negative positive or neutral states.

--- Source/AgentSlickUE410/AgentSLICK_BPFunctionLibrary.h
+++ Source/AgentSlickUE410/AgentSLICK_BPFunctionLibrary.h
@@ -21,6 +21,42 @@ public:
 
 };
 
+USTRUCT(BlueprintType)
+struct FGuardChatWithTuxedo : public FTableRowBase
+{
+	GENERATED_USTRUCT_BODY()
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Chatting System")
+		FText Guard;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Chatting System")
+		FText Greet;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Chatting System")
+		FText Distraction;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Chatting System")
+		FText Danswer;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Chatting System")
+		int32 Dtype;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Chatting System")
+		FText Joke;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Chatting System")
+		FText Janswer;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Chatting System")
+		int32 Jtype;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Chatting System")
+		FText Charm;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Chatting System")
+		FText Canswer;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Chatting System")
+		int32 Ctype;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Chatting System")
+		FText Story;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Chatting System")
+		FText Sanswer;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Chatting System")
+		int32 Stype;
+};
+
+
 UCLASS()
 class AGENTSLICKUE410_API UAgentSLICK_BPFunctionLibrary : public UBlueprintFunctionLibrary
 {
